fce6d78e601bfc8c48becba8b66e2e5d1a866a70
Andrew Pearson <s195206@uos.ac.uk>
2018-11-25 14:35:40 +0000

    test github

--- ParserAssignment/src/http/GetRequester.java
+++ ParserAssignment/src/http/GetRequester.java
@@ -1,9 +1,51 @@
 package http;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
 public class GetRequester {
 
-	public GetRequester() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) throws Exception {
+
+		GetRequester http = new GetRequester();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+
 	}
 
-}
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "http://i2j.openode.io/task/5447";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+	}
